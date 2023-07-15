82a6524f2171373432525596981c873679a13684
Kiran Anand <liteninkiran@hotmail.com>
2023-07-15 23:51:20 +0100

    36 - Validation & store listing

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
@@ -30,7 +31,25 @@ class ListingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required',
+        ]);
+
+        // if($request->hasFile('logo')) {
+        //     $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        // }
+
+        // $formFields['user_id'] = auth()->id();
+
+        Listing::create($formFields);
+
+        return redirect('/listings')->with('message', 'Listing created successfully!');
     }
 
     /**
