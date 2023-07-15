ead785b6a271c727339af41289e442486a6eb852
Kiran Anand <liteninkiran@hotmail.com>
2023-07-15 15:09:47 +0100

    11 - View basics and passing data

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', function () {
-    return response('<h1>Hello World</h1>')
-        ->header('Content-Type', 'text/plain')
-        ->header('foo', 'bar');
-});
-
-Route::get('/posts/{id}', function ($id) {
-    return response('Post ' . $id);
-})->where('id', '[0-9]+');
-
-Route::get('/search', function (Request $request) {
-    return $request->name . ' - ' . $request->city;
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing 1',
+                'description' => '.jgflsfsdf',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing 2',
+                'description' => 'gfhjldkfjhgfhj',
+            ],
+        ],
+    ]);
 });
