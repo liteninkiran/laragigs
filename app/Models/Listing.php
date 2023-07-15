460cd50c1b1bc278f672968e5a46edf3bdc04107
Kiran Anand <liteninkiran@hotmail.com>
2023-07-15 19:14:00 +0100

    19 - Create an eloquent model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,21 +9,6 @@ class Listing extends Model
 {
     use HasFactory;
 
-    public static function all($columns = ['*']) {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing 1',
-                'description' => 'jgflsfsdf',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing 2',
-                'description' => 'gfhjldkfjhgfhj',
-            ],
-        ];
-    }
-
     public static function find($id) {
         $listings = self::all();
         foreach ($listings as $listing) {
